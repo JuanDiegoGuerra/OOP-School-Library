72f00dabedbdda198bc96bb2f58697efe31b4d94
Juan Diego Guerra <jdxelric@gmail.com>
2023-10-06 02:05:28 -0500

    Update teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+  def initialize(age, specialization, name: 'Unknown', parent_permission: true)
+    super(age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
