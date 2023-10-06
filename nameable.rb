f5a5538ec47ecdb39ab6f36015fafb5dda8153a8
Juan Diego Guerra <jdxelric@gmail.com>
2023-10-05 20:09:10 -0500

    Fixing Linters

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,5 @@
 class Nameable
-    def correct_name
-      raise NotImplementedError, 'Subclasses must implement the correct_name method.'
-    end
+  def correct_name
+    raise NotImplementedError, 'Subclasses must implement the correct_name method.'
   end
+end
