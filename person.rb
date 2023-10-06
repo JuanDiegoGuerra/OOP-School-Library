f2a6825d53a5ce0ac049ab18f2817ca34a4155c4
Juan Diego Guerra <jdxelric@gmail.com>
2023-10-05 21:54:24 -0500

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,14 +1,16 @@
 require_relative 'nameable'
+require_relative 'rental'
 
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = generate_id
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
     super()
   end
 
@@ -18,6 +20,10 @@ class Person < Nameable
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, self, book)
   end
 
   private
