acc1e0c23f22c63e588eac7f3d29a3ceb5792148
Juan Diego Guerra <jdxelric@gmail.com>
2023-10-11 09:25:30 -0500

    Update student.rb
    Co-Authored-By: Hafiz Rasa <117447018+hafiz1379@users.noreply.github.com>

--- student.rb
+++ student.rb
@@ -1,11 +1,12 @@
 require_relative 'person'
 
 class Student < Person
-  attr_reader :classroom
+  attr_reader :classroom, :json_class, :parent_permission
 
-  def initialize(age, classroom: nil, name: 'Unknown', parent_permission: true)
+  def initialize(age, classroom: nil, name: 'Unknown', parent_permission: true, json_class: 'Student')
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
+    @json_class = json_class
     classroom.students << self unless classroom.nil?
   end
 
